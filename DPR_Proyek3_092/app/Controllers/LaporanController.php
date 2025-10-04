b0d2e37b48dd8bf183ddc0ca017966dd86f52703
wyandhanupapoy <wyandhanupapoy@gmail.com>
2025-10-04 18:58:15 +0700

    Add Bonus Fitur Ubah Data Penggajian by Wyandhanu Maulidan Nugraha

--- DPR_Proyek3_092/app/Controllers/LaporanController.php
+++ DPR_Proyek3_092/app/Controllers/LaporanController.php
@@ -10,9 +10,9 @@ class LaporanController extends BaseController
     public function index()
     {
         $anggotaModel = new AnggotaModel();
-
+        
         $data = [
-            'all_anggota' => $anggotaModel->findAll(), // Untuk mengisi dropdown
+            'all_anggota' => $anggotaModel->findAll(),
             'detail_gaji' => [],
             'anggota_terpilih' => null
         ];
@@ -20,22 +20,10 @@ class LaporanController extends BaseController
         // Cek jika ada anggota yang dipilih dari form
         $id_anggota = $this->request->getPost('id_anggota');
         if ($id_anggota) {
-            // Panggil fungsi cerdas yang kita buat di model
             $data['detail_gaji'] = $anggotaModel->getGajiDetail($id_anggota);
-            // Ambil data anggota yang dipilih untuk ditampilkan
             $data['anggota_terpilih'] = $anggotaModel->find($id_anggota);
         }
 
         return view('laporan/index', $data);
-    }
-
-    public function gajiFinal()
-    {
-        $anggotaModel = new AnggotaModel();
-
-        // Panggil fungsi query cerdas yang baru kita buat
-        $data['laporan_gaji'] = $anggotaModel->getLaporanGaji();
-
-        return view('laporan/gaji_final', $data);
     }
 }
--- DPR_Proyek3_092/app/Views/laporan/index.php
+++ DPR_Proyek3_092/app/Views/laporan/index.php
@@ -18,38 +18,41 @@
             <?php endforeach; ?>
         </select>
     </form>
-
     <hr>
-
-    <?php if (!empty($detail_gaji)): ?>
+    <?php if ($anggota_terpilih): ?>
         <h3>Rincian untuk: <?= esc($anggota_terpilih['nama_depan'] . ' ' . $anggota_terpilih['nama_belakang']) ?></h3>
-        <table border="1" cellpadding="5" cellspacing="0">
-            <thead>
-                <tr>
-                    <th>Komponen Gaji</th>
-                    <th>Nominal</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php $total = 0; ?>
-                <?php foreach($detail_gaji as $item): ?>
-                <tr>
-                    <td><?= esc($item['nama_komponen']) ?></td>
-                    <td align="right"><?= number_format($item['nominal'], 2, ',', '.') ?></td>
-                </tr>
-                <?php $total += $item['nominal']; ?>
-                <?php endforeach; ?>
-            </tbody>
-            <tfoot>
-                <tr>
-                    <th>TOTAL</th>
-                    <th align="right"><?= number_format($total, 2, ',', '.') ?></th>
-                </tr>
-            </tfoot>
-        </table>
+
+        <?php if (!empty($detail_gaji)): ?>
+            <table border="1" cellpadding="5" cellspacing="0">
+                <thead>
+                    <tr>
+                        <th>Komponen Gaji</th>
+                        <th>Nominal</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php $total = 0; ?>
+                    <?php foreach($detail_gaji as $item): ?>
+                    <tr>
+                        <td><?= esc($item['nama_komponen']) ?></td>
+                        <td align="right"><?= number_format($item['nominal'], 2, ',', '.') ?></td>
+                    </tr>
+                    <?php $total += $item['nominal']; ?>
+                    <?php endforeach; ?>
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <th>TOTAL</th>
+                        <th align="right"><?= number_format($total, 2, ',', '.') ?></th>
+                    </tr>
+                </tfoot>
+            </table>
+        <?php else: ?>
+            <p>Anggota ini belum memiliki komponen gaji yang diatur.</p>
+        <?php endif; ?>
+
     <?php else: ?>
         <p>Silakan pilih anggota untuk melihat rincian gajinya.</p>
     <?php endif; ?>
-
 </body>
 </html>
